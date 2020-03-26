923f1b395df6dc5d0a68efd55c3d07b0612bec83
wanxiuhua <wanxiuhua@kedacom.com>
2020-03-26 11:24:12 +0800

    发送消息优化

--- src/components/mainPage/page1.vue
+++ src/components/mainPage/page1.vue
@@ -4,7 +4,10 @@
     <div class="page-content">
       <iframe :src="src" frameborder='0' scrolling="auto" height="30%"  width="40%"></iframe>
       <hr size=5>
+      <div style="display:flex;justify-content:center">
+      <el-input style="width:200px;margin-right:10px" v-model='info' placeholder="请输入要发送的消息"></el-input>
       <el-button type="priamry" @click="sendInfo">给窗口2发送消息</el-button> 
+      </div>
     </div>
   </div>
 </template>
@@ -13,7 +16,8 @@ import {postData} from '@/utils/event-bus.js'
 export default {
   data(){
     return{
-      src:'https://note.youdao.com/web/#/file/recent/note/WEB79adcf16d779cc67d8886ef5198e2841/'
+      src:'https://note.youdao.com/web/#/file/recent/note/WEB79adcf16d779cc67d8886ef5198e2841/',
+      info:'',
     }
   },
   mounted(){
@@ -22,7 +26,7 @@ export default {
   methods:{
      sendInfo(){
       //  window.postMessage('我是来自页面1的消息，请查收','http://localhost:8081/#/page2')
-     postData("我是来自页面1的消息，请查收")
+     postData(this.info)
      }
   }
 }
